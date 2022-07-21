bcedcaa0234ee14de3c9be1eeb201272f7c80aab
Andres Sanchez Salcedo <andres.sanchezz117@gmail.com>
2022-07-21 00:57:20 -0500

    Agregar rutas para carreras y login

---
+++ databaseService.js
@@ -0,0 +1,69 @@
+const databaseService = () => {
+  const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      port: 3306,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB
+    }
+  })
+
+  // LOGIN
+  const login = ({ correo_estudiante, contrasena }) => {
+    return knex('Estudiante').where({
+      correo_estudiante: correo_estudiante,
+      contrasena: contrasena,
+      estado: 0
+    }).select()
+  }
+
+  // ESTUDIANTE
+
+  const getEstudiante = () => {
+
+  }
+
+  const postEstudiante = ({ codigo, nombre, primer_apellido, segundo_apellido, contrasena, clave_carrera, ciclo_escolar, num_semestre, estatus, correo_estudiante }) => {
+    return knex('Estudiante').insert({
+      codigo: codigo,
+      nombre: nombre,
+      primer_apellido: primer_apellido,
+      segundo_apellido: segundo_apellido,
+      contrasena: contrasena,
+      clave_carrera: clave_carrera,
+      ciclo_escolar: ciclo_escolar,
+      num_semestre: num_semestre,
+      estatus: estatus,
+      correo_estudiante: correo_estudiante
+    })
+  }
+
+  // CARRERAS
+
+  const getCarreras = () => {
+    return knex('Carrera').where({
+      estado: 0
+    }).select('nombre', 'clave')
+  }
+
+  const postCarreras = ({ nombre, clave }) => {
+    return knex('Carrera').insert({
+      nombre: nombre,
+      clave: clave
+    })
+  }
+
+  return {
+    login,
+    getEstudiante,
+    postEstudiante,
+    getCarreras,
+    postCarreras
+  }
+}
+
+module.exports = {
+  databaseService
+}
---
+++ server_mysql.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+
+const express = require('express')
+const bp = require('body-parser')
+const morgan = require('morgan')
+const { databaseService } = require('./databaseService')
+
+const app = express()
+const dbService = databaseService()
+
+app.use(bp.urlencoded({ extended: true }))
+app.use(bp.json())
+app.use(morgan('dev'))
+
+// ROUTES
+
+app.get('/login', (req, res) => {
+  dbService.login(req.body)
+    .then(estudiante => res.json(estudiante))
+    .catch(e => res.status(500).send(e))
+})
+
+app.post('/estudiante', (req, res) => {
+  dbService.postEstudiante(req.body)
+    .then(() => res.json({ mensaje: "Estudiante creado." }))
+    .catch(e => res.status(500).send(e))
+})
+
+app.get('/carreras', (req, res) => {
+  dbService.getCarreras()
+    .then(carreras => res.json(carreras))
+    .catch(e => res.status(500).send(e))
+})
+
+app.post('/carreras', (req, res) => {
+  dbService.postCarreras(req.body)
+    .then(() => res.json({ mensaje: "Carrera creada." }))
+    .catch(e => res.status(500).send(e))
+})
+
+app.listen(8000, () => {
+  console.log('Server on http://localhost:8000')
+})
